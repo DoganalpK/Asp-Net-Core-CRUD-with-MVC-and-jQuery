35bbcb63c0cf91b5725b5bbad7efdd597bc6a64c
Doğanalp KARATAŞ <doganalpkaratas@gmail.com>
2022-07-22 09:35:35 +0300

    -packages downl. from nuget -create db connection

---
+++ dotNetCore-CRUD-MVC/Models/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotNetCore_CRUD_MVC.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- dotNetCore-CRUD-MVC/Program.cs
+++ dotNetCore-CRUD-MVC/Program.cs
@@ -1,7 +1,13 @@
+using dotNetCore_CRUD_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
@@ -12,6 +18,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
